c1ee913e27a7254939295f44612352293a319c88
Alekhya <alekhya@ubuntu.(none)>
2013-01-15 12:23:28 +0300

    initial commit

---
+++ code/bluebook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluebook::Application.config.secret_token = 'd2cf84c2ea32a712cc7dd683b97c9e1473a3eedf5e5204ef472e543e5dd4908ff1baded3ca897aec019747738ad22ad9295993483c2defe702f63f2e9c7d603a'
